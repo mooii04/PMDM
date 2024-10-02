15e5ced179e37986809067d9e063b4331d00949d
MoisésDorado <dorado.gumoi24@triana.salesianos.edu>
2024-10-02 18:58:36 +0200

    Ejercicio de tablas

---
+++ EjercicioJQuery-Visual/03_tablas/03_tablas.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $(document).on("click", "#btn-add-row", function () {
+        var row = "<tr><td>Nuevo</td></tr>";
+        $("#table").append(row);
+    });
+
+    $(document).on("click", "#btn-add-column", function () {
+        $("#table tr").each(function () {
+            $(this).append("<td>Nuevo</td>");
+        });
+    });
+
+    $(document).on("click", "#btn-delete-row", function () {
+        $("#table tr:last").remove();
+    });
+
+    $(document).on("click", "#btn-delete-column", function () {
+        $("#table tr").each(function () {
+            $(this).children().last().remove();
+        });
+    });
+});
