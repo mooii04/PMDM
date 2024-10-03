b4551a39711ebd371f0540d1c46104dd35491585
Dorado <dorado.gumoi24@triana.salesianos.edu>
2024-10-03 10:11:40 +0200

    Miguel

---
+++ EjercicioJQuery-Visual/asincrono/asincrono.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $.ajax({
+        url: "https://swapi.dev/api/people",
+        method: "GET",
+    }).done(function (data) {
+        debugger; 
+        //JSON.parse(data) convierte el texto en formato JSON a un objeto de JavaScript
+        var json = JSON.parse(data);
+        debbuger;
+    
+        /*
+        Lo que se programa dentro de esta función se ejecutará
+        cuando se haya resuleto la petición asíncroma, es decir,
+        cuando llegue la repsuesta del servidor
+        */
+    });
+
+});
