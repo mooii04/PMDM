06f583bf1f1e3a4eba40ab76718f386862319815
Dorado <dorado.gumoi24@triana.salesianos.edu>
2024-10-15 12:30:52 +0200

    asasd

--- calculadora/src/app/components/calculadora/calculadora.component.ts
+++ calculadora/src/app/components/calculadora/calculadora.component.ts
@@ -6,15 +6,47 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
-  addNum: number = 0;
-  result: number = 0;
-  sumar() {
-    this.result = this.addNum + this.addNum;  
+  operacion = '';
+  sumarpulsado = false;
+  restarpulsado = false;
+  pulsado = false;
+  num1 = 0;
+  num2 = 0;
+  resultado = 0;
+
+  guardar(arg0: number) {
+    if (!this.pulsado) {
+      this.num1 = arg0;
+      this.pulsado = true;
+    } else {
+      this.num2 = arg0;
+      this.pulsado = false;
+    }
   }
 
   restar() {
-    //this.result = this.addNum - this.addNum;
+    this.restarpulsado = true;
+    this.operacion = '-';
+  }
+  sumar() {
+    this.sumarpulsado = true;
+    this.operacion = '+';
   }
 
+  mostrarResultado() {
+    if (this.sumarpulsado) {
+      this.resultado = this.num1 + this.num2;
+      this.sumarpulsado = false;
+    } else if (this.restarpulsado) {
+      this.resultado = this.num1 - this.num2;
+      this.restarpulsado = false;
+    }
+  }
+
+  delete() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.resultado = 0;
+  }
 
 }
