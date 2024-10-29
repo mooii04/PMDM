149e258c9ce34fd7c19a33c87e4a2b8cadf9b94a
MoisésDorado <dorado.gumoi24@triana.salesianos.edu>
2024-10-29 20:41:43 +0100

    batle mas o menos

--- pokemon-battle-michy/src/app/components/pokemon/pokemon.component.ts
+++ pokemon-battle-michy/src/app/components/pokemon/pokemon.component.ts
@@ -1,40 +1,64 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { PokemonResponse } from '../../models/pokemon.interface';
 import { AnimationOptions } from 'ngx-lottie';
-import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrl: './pokemon.component.css'
+  styleUrl: './pokemon.component.css',
 })
 export class PokemonComponent implements OnInit {
 
   options: AnimationOptions = {
-    path: '/assets/animation.json',
+    path: '/assets/animation_explosion.json',
   };
 
   @Input() pokemonId: number | undefined;
   pokemon: PokemonResponse | undefined;
+  pokemonGanador: PokemonResponse | undefined;
   @Input() life: number = 100;
-  @Output() onAttack = new EventEmitter<number>();
+  @Output() onAttackDone = new EventEmitter<number>();
+  @Output() onHealDone = new EventEmitter<number>();
   @Input() isMyTurn: boolean = false;
+  showAnimation: boolean = false;
+  @Input() terminado: boolean = false;
+  batallaTerminada = false;
+  curaCount = 0;
+  curasQuedan = 3;
 
   constructor(private pokemonService: PokemonService) { }
 
-  //? con la interrogación se pregunta si el valor es nulo o indefinido
-  //! con la exclamaicon se asegura que el valor no es nulo o indefinido
   ngOnInit(): void {
-    this.pokemonService.getPokemon(this.pokemonId!).subscribe(pokemonResponse => {
-      this.pokemon = pokemonResponse;
-    });
+    this.pokemonService
+      .getPokemon(this.pokemonId!)
+      .subscribe((pokemonResponse) => {
+        this.pokemon = pokemonResponse;
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['life']) {
+      if (changes['life'].firstChange == false) {
+        this.showAnimation = true;
+        setTimeout(() => {
+          this.showAnimation = false;
+        }, 500);
+      }
+    }
   }
 
   getProgressBarColor(): string {
-    if (this.life > 60) {
+    if (this.life >= 70) {
       return 'success';
-    } else if (this.life > 40) {
+    } else if (this.life >= 40) {
       return 'warning';
     } else {
       return 'danger';
@@ -42,8 +66,26 @@ export class PokemonComponent implements OnInit {
   }
 
   doAttack() {
-    var damage = Math.floor(Math.random() * (30 - 10)) + 10;
-    this.onAttack.emit(damage);
+    var damage = Math.floor(Math.random() * (30 - 10) + 10);
+    this.onAttackDone.emit(damage);
+
+  }
+
+  curar() {
+    if (this.curaCount < 3 && this.life < 100) {
+      this.curasQuedan--;
+      this.curaCount++;
+      var curaAmount = Math.floor(Math.random() * ((100 - this.life) - 10) + 10);
+      this.onHealDone.emit(curaAmount);
+
+    }
+
+    if (this.life > 100) {
+      this.life = 100;
+    }
+
+
+
   }
 
 }
